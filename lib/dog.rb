283ec9457f5267668c4b96f773f29f8f8aa5814f
Joshua Williams <jmw0426@hotmail.com>
2020-05-22 20:42:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,24 @@
 class Dog
+  attr_accessor :dogs_name
   
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+  
+  # def name=(dogs_name)
+  #   @this_dogs_name = dogs_name
+  # end
+  
+  # def name
+  #   @this_dogs_name
+  # end
+  
+  def initialize
+    @dogs_name = dogs_name
   end
   
-  def name
-    @this_dogs_name
-  end
-  
-  def bark 
+  def bark(dogs_name) 
     puts "Woof!"
   end
   
-  def sit 
+  def sit(dogs_name) 
     puts "The Dog is sitting"
   end
 end
